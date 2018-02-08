e073e11b774644b28d72cabe39b9cb262a6bb95f
yezuoyi <181639900@qq.com>
2018-02-08 14:17:40 +0800

    frist commit how to use makefile demo

--- build/Vector.d
+++
@@ -1,3 +0,0 @@
-build/Vector.o: src/Vector.cpp include/project/Vector.hpp
-
-include/project/Vector.hpp:
--- build/main.d
+++
@@ -1,3 +0,0 @@
-build/main.o: src/main.cpp include/project/Vector.hpp
-
-include/project/Vector.hpp:
